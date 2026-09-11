395a1af2b96ba1ed72acab2b7bf7d8bfb0a15550
Raghav Kacker <raghavkacker66991@gmail.com>
2026-09-11 23:41:36 +0530

    updated model

--- frontend/src/components/map/SonarImageViewer.tsx
+++ frontend/src/components/map/SonarImageViewer.tsx
@@ -11,7 +11,7 @@ import { RISK_COLORS } from '../../utils/colorScale';
 const CLASS_LABELS: Record<string, string> = {
   crab_pot: 'Crab Pot',
   ghost_gear: 'Ghost Gear',
-  mine_cylinder: 'Underwater_pipeline',
+  mine_cylinder: 'Mine / Cylinder',
   debris_anomaly: 'Debris Anomaly',
 };
 
